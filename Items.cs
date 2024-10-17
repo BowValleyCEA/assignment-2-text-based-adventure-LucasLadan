2e5f892bd630e8a195bfb3faab26a510740f90da
Lucas Ladan <l.ladan419@mybvc.ca>
2024-10-17 16:49:39 -0600

    Setup the rooms and items

---
+++ Items.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game1402_a2_starter
+{
+    [Serializable]
+    internal class Items
+    {
+        public string Name { get; set; }
+
+        public string reference { get; set; }
+
+        public 
+
+    }
+}
--- Room.cs
+++ Room.cs
@@ -12,5 +12,6 @@ namespace game1402_a2_starter
         public string Name { get; set; }
         public string Reference { get; set; }
         public string Description { get; set; }
+        public bool isUnlocked { get; set; }
     }
 }
